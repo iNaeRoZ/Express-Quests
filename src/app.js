f2998bfbc3befe6426c156949aab0bf2b20d3ab7
iNaeRoZ <naeroz@outlook.fr>
2023-11-30 02:14:29 +0100

    express 2.0

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(express.json());
+
 const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
@@ -11,8 +13,10 @@ app.get("/api/movies/:id", movieControllers.getMovieById);
 const userControllers = require("./controllers/userControllers");
 
 app.get("/api/users", userControllers.getUsers);
-app.get("/api/users/:id", userControllers.getUserById)
+app.get("/api/users/:id", userControllers.getUserById);
 
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", userControllers.postUsers);
 const port = process.env.APP_PORT;
 
 module.exports = app;
--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -30,9 +30,27 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      // wait for it
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 
 
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie
 };
